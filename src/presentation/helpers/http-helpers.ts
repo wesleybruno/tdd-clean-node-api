07130248bb591f7daa91aa3ebfbfca5b70fef915
Wesley <wesleybruno.so@gmail.comm>
2021-06-02 19:44:54 -0300

    feat: internal error, internal server error

--- src/presentation/helpers/http-helpers.ts
+++ src/presentation/helpers/http-helpers.ts
@@ -4,3 +4,8 @@ export const badRequest = (body: Error): HttpResponse => ({
   statusCode: 400,
   body: body
 })
+
+export const internalServerError = (body: Error): HttpResponse => ({
+  statusCode: 500,
+  body: body
+})
