e7aaa38d844eb46195e17b22ade0408deb5bdf61
Wesley <wesleybruno.so@gmail.comm>
2021-06-03 18:31:29 -0300

    test: teste email validator

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -1,9 +1,18 @@
+import validator from 'validator'
 import { EmailValidatorAdapter } from './email-validator-adapter'
 
 describe('Eamil Validator Adapter', () => {
   test('Should return false if validator return false', () => {
     const sut = new EmailValidatorAdapter()
+    jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
     const isValid = sut.isValid('invalid_email')
     expect(isValid).toBe(false)
+  })
+
+  test('Should return true if validator return false', () => {
+    const sut = new EmailValidatorAdapter()
+    jest.spyOn(validator, 'isEmail').mockReturnValueOnce(true)
+    const isValid = sut.isValid('valid_email@gmail.com')
+    expect(isValid).toBe(true)
   })
 })
