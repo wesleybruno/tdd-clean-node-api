99dba7a176745ebab8ed7742aa49cb8da05050ee
Wesley <wesleybruno.so@gmail.comm>
2021-06-03 18:31:04 -0300

    feat: email validator

--- src/utils/email-validator-adapter.ts
+++ src/utils/email-validator-adapter.ts
@@ -1,7 +1,7 @@
+import validator from 'validator'
 import { EmailValidator } from '../presentation/protocols'
-
 export class EmailValidatorAdapter implements EmailValidator {
-  isValid (email: String): boolean {
-    return false
+  isValid (email: string): boolean {
+    return validator.isEmail(email)
   }
 }
