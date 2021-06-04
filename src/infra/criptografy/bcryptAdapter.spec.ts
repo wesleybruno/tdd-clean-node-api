d59b27d767f589a495e7ddd30db49f42e493456f
Wesley <wesleybruno.so@gmail.comm>
2021-06-04 11:16:13 -0300

    test: test return hashed string

--- src/infra/criptografy/bcryptAdapter.spec.ts
+++ src/infra/criptografy/bcryptAdapter.spec.ts
@@ -1,6 +1,12 @@
 import bcrypt from 'bcrypt'
 import { BcryptAdapter } from './bcryptAdapter'
 
+jest.mock('bcrypt', () => ({
+  async hash (): Promise<string> {
+    return await Promise.resolve('hashed_string')
+  }
+}))
+
 describe('Bcrypt Adapter', () => {
   test('Should call bcrypt with correct value', async () => {
     const salt = 12
@@ -11,5 +17,14 @@ describe('Bcrypt Adapter', () => {
     await sut.encrypt('any_value')
 
     expect(hashSpy).toHaveBeenCalledWith('any_value', salt)
+  })
+
+  test('Should return a hash on success', async () => {
+    const salt = 12
+    const sut = new BcryptAdapter(salt)
+
+    const hash = await sut.encrypt('any_value')
+
+    expect(hash).toBe('hashed_string')
   })
 })
