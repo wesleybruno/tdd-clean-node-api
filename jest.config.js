87e2e672b12712a0c68f858a61fa6d13d7ff7007
Wesley <wesleybruno.so@gmail.comm>
2021-06-05 23:09:47 -0300

    fix: jest config

--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,14 @@ module.exports = {
   },
   clearMocks: true,
   collectCoverage: false,
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!<rootDir>/src/main/**'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main/**',
+    '!<rootDir>/src/data/protocols/**',
+    '!<rootDir>/src/presentation/protocols/**',
+    '!<rootDir>/src/domain/**',
+    '!<rootDir>/src/**/*protocols.ts'
+  ],
   coverageDirectory: 'coverage',
   coverageProvider: 'v8',
   preset: '@shelf/jest-mongodb'
